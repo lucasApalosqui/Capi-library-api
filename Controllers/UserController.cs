f39c51aa3222378e76448720ce1ab1bef6e3a286
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-03-05 16:08:23 -0300

    :sparkles: feat: GetAllUsers Endpoint

---
+++ Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Capi_Library_Api.Data;
+using Capi_Library_Api.ViewModels;
+using Capi_Library_Api.ViewModels.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Capi_Library_Api.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("v1/users")]
+        public async Task<IActionResult> GetAllUsers([FromServices] DataContext context)
+        {
+            try
+            {
+                var users = await context.Users
+                    .AsNoTracking()
+                    .Include(x => x.Role)
+                    .Select(x => new GetAllUserViewModel
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Email = x.Email,
+                        Cpf = x.Cpf,
+                        Role = x.Role.Name
+                    })
+                    .OrderBy(x => x.Name)
+                    .ToListAsync();
+
+                return Ok(new ResultViewModel<List<GetAllUserViewModel>>(users));
+            } catch (Exception ex)
+            {
+                return NotFound(new ResultViewModel<GetAllUserViewModel>("87650 - Usuarios Não encontrados"));
+            }
+        }
+    }
+}
---
+++ ViewModels/Users/GetAllUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Capi_Library_Api.ViewModels.Users
+{
+    public class GetAllUserViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Role { get; set; }
+    }
+}
