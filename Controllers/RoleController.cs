1689ce2c8919ad34d2d53ebd12ba1cba84f75ef7
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-03-05 14:56:16 -0300

    :sparkles: feat: RoleController and ViewModel

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,28 @@
+﻿using Capi_Library_Api.Data;
+using Capi_Library_Api.ViewModels.Roles;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Capi_Library_Api.Controllers
+{
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+
+        [HttpGet("v1/roles")]
+        public async Task<IActionResult> GetAllRolesAsync([FromServices] DataContext context)
+        {
+            List<GetAllRolesViewModel> rolesV = new List<GetAllRolesViewModel>();
+            
+            var roles = await context.Roles.ToListAsync();
+            foreach (var role in roles)
+            {
+                GetAllRolesViewModel roleView = new GetAllRolesViewModel();
+                roleView.Id = role.Id;
+                roleView.Name = role.Name;
+                rolesV.Add(roleView);
+            }
+            return Ok(rolesV);
+        }
+    }
+}
---
+++ ViewModels/Roles/GetAllRolesViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Capi_Library_Api.ViewModels.Roles
+{
+    public class GetAllRolesViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
