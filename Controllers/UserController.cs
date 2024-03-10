9b6cca0509f31e005d7a69b96dda9b269cb95756
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-03-10 18:08:23 -0300

    :sparkles: feat: GetUserByEmail

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -80,8 +80,60 @@ namespace Capi_Library_Api.Controllers
             {
                 return NotFound(new ResultViewModel<GetUserProfileViewModel>("78650 - Não encontrado"));
             }
+       
+        }
 
-                
+        [Authorize(Roles = "Admin")]
+        [HttpGet("v1/users/{email}")]
+        public async Task<IActionResult> GetUserByEmail([FromServices] DataContext context, [FromRoute] string email)
+        {
+            try
+            {
+                var user = await context.Users
+                .Include(x => x.Address)
+                .Include(x => x.Role)
+                .Include(x => x.Phones)
+                .Include(x => x.Rental)
+                .ThenInclude(x => x.RentalBook)
+                .FirstOrDefaultAsync(x => x.Email == email);
+
+                if (user == null)
+                {
+                    return NotFound(new ResultViewModel<GetUserByEmailViewModel>("78650 - Usuário não encontrado"));
+                }
+
+                IList<string> phoneS = new List<string>();
+                foreach (var phone in user.Phones)
+                {
+                    phoneS.Add(phone.PhoneNumber);
+                }
+
+                var userE = new GetUserByEmailViewModel
+                {
+                    Name = user.Name,
+                    Email = user.Email,
+                    Cpf = user.Cpf,
+                    street = user.Address.Street,
+                    Disctrict = user.Address.Disctrict,
+                    State = user.Address.State,
+                    Number = user.Address.Number,
+                    Complement = user.Address.Complement,
+                    phone = phoneS,
+                    Role = user.Role.Name
+                };
+
+                if (user.Rental != null)
+                {
+                    userE.RentalId = user.Rental.Id;
+                    userE.book = user.Rental.RentalBook.Title;
+                    userE.GetDate = user.Rental.GetDate;
+                    userE.ReturnDate = user.Rental.ReturnDate;
+                }
+                return Ok(new ResultViewModel<GetUserByEmailViewModel>(userE));
+            }catch (Exception ex)
+            {
+                return NotFound(new ResultViewModel<GetUserProfileViewModel>("55650 - Não encontrado"));
+            }     
         }
     }
 }
---
+++ ViewModels/Users/GetUserByEmailViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Capi_Library_Api.ViewModels.Users
+{
+    public class GetUserByEmailViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Role { get; set; }
+        public string street { get; set; }
+        public string Disctrict { get; set; }
+        public string State { get; set; }
+        public int Number { get; set; }
+        public string Complement { get; set; }
+        public IList<string> phone { get; set; }
+
+        public int? RentalId { get; set; }
+        public string? book { get; set; }
+        public DateTime? GetDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
