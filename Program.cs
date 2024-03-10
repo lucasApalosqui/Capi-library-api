422bdd6049778d0ba921ba6770ae64643c4a89ea
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-03-09 22:06:11 -0300

    :sparkles: feat: Token Validation Configure

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 using Capi_Library_Api;
 using Capi_Library_Api.Data;
 using Capi_Library_Api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-
+ConfigureAuthentication(builder);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,9 +19,12 @@ builder.Services.AddDbContext<DataContext>(options =>
     string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
     options.UseSqlServer(connectionString);
 });
+builder.Services.AddTransient<TokenService>();
 
 
 var app = builder.Build();
+
+
 
 
 if (app.Environment.IsDevelopment())
@@ -34,3 +40,27 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+   
+}
