f6efeff89907b6faa4d1fe662624835b341bcaea
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-03-09 21:39:12 -0300

    :sparkles: feat: Account Register

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Capi_Library_Api.Data;
+using Capi_Library_Api.Models;
+using Capi_Library_Api.ViewModels;
+using Capi_Library_Api.ViewModels.Accounts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
+
+namespace Capi_Library_Api.Controllers
+{
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("v1/accounts/register")]
+        public async Task<IActionResult> Post([FromBody] RegisterAccountViewModel model, [FromServices] DataContext context)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var userVeri = await context.Users.FirstOrDefaultAsync(x => x.Email == model.Email);
+
+            if (userVeri != null)
+            {
+                return StatusCode(400, new ResultViewModel<string>("05X77 - Email já está sendo utilizado"));
+            }
+
+            var user = new User
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Cpf = model.Cpf             
+            };
+
+            var role = context.Roles.FirstOrDefault(x => x.Id == 1);
+
+            user.Role = role;
+            user.PasswordHash = PasswordHasher.Hash(model.Password);
+
+            try
+            {
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+
+                return Ok();
+            }catch (Exception ex)
+            {
+                return StatusCode(404, new ResultViewModel<string>(ex.ToString()));
+            }
+        }
+    }
+}
---
+++ ViewModels/Accounts/RegisterAccountViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Capi_Library_Api.ViewModels.Accounts
+{
+    public class RegisterAccountViewModel
+    {
+        [Required(ErrorMessage = "Nome obrigatório")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email obrigatório")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha obrigatória")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Cpf obrigatório")]
+        public string Cpf {  get; set; }
+
+    }
+}
