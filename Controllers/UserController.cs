c9b1e0ce8280a1a86148aaaaacfb026a062aa1d5
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-03-10 17:28:26 -0300

    :sparkles: feat: GetMyProfile

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,6 +6,7 @@ using Capi_Library_Api.ViewModels.Users;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace Capi_Library_Api.Controllers
 {
@@ -38,6 +39,49 @@ namespace Capi_Library_Api.Controllers
             {
                 return NotFound(new ResultViewModel<GetAllUserViewModel>("87650 - Usuarios Não encontrados"));
             }
+        }
+
+        [Authorize(Roles = "Student")]
+        [HttpGet("v1/users/my-profile")]
+        public async Task<IActionResult> GetUser([FromServices] DataContext context)
+        {
+            try
+            {
+                var email = HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+
+                var user = await context.Users
+                    .Include(x => x.Address)
+                    .Include(x => x.Role)
+                    .Include(x => x.Phones)
+                    .FirstOrDefaultAsync(x => x.Email == email);
+
+                IList<string> phoneS = new List<string>();
+                foreach(var phone in user.Phones)
+                {
+                    phoneS.Add(phone.PhoneNumber);
+                }
+                var userAll = new GetUserProfileViewModel
+                {
+                    Name = user.Name,
+                    Email = user.Email,
+                    Cpf = user.Cpf,
+                    street = user.Address.Street,
+                    Disctrict = user.Address.Disctrict,
+                    State = user.Address.State,
+                    Number = user.Address.Number,
+                    Complement = user.Address.Complement,
+                    phone = phoneS,
+                    Role = user.Role.Name
+
+                };
+
+                return Ok(new ResultViewModel<GetUserProfileViewModel>(userAll));
+            }catch (Exception ex)
+            {
+                return NotFound(new ResultViewModel<GetUserProfileViewModel>("78650 - Não encontrado"));
+            }
+
+                
         }
     }
 }
---
+++ ViewModels/Users/GetUserProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Capi_Library_Api.ViewModels.Users
+{
+    public class GetUserProfileViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Role { get; set; }
+        public string street { get; set; }
+        public string Disctrict { get; set; }
+        public string State { get; set; }
+        public int Number { get; set; }
+        public string Complement { get; set; }
+        public IList<string> phone { get; set; }
+    }
+}
